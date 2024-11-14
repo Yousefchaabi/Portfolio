439205f96b078a6d0678e6640997ec944d73d8f5
Yousefchaabi <92028687+Yousefchaabi@users.noreply.github.com>
2024-11-15 00:04:24 +0100

    another update

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Yousefchaabi",
   resume:
-    "https://drive.google.com/file/d/1Mt4DbAyXLuAV2vSLCcmgi0UPidMZxw2R/view?usp=sharing",
+    "https://docs.google.com/document/d/1QvzL5dE9ExHMpb5W7HwIDZfjkIWSpu1y3t-MPBOx_z8/edit?usp=sharing",
   linkedin: "https://www.linkedin.com/in/youssef-chaabi-863510187/",
   insta: "https://www.instagram.com/youssef_chaabii/",
   facebook: "https://www.facebook.com/youssef.chaabi.9634/",
